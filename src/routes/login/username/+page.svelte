c934f3574c85aa7bc5eae6feee5d54f9cadfc053
Liken Tan <likendev@gmail.com>
2023-10-04 02:40:47 +0800

    feat: advanced features
    - Atomic Writes

--- src/routes/login/username/+page.svelte
+++ src/routes/login/username/+page.svelte
@@ -1,9 +1,9 @@
-<script lang="ts">
-  import AuthCheck from "$lib/components/AuthCheck.svelte";
-  import { db, user } from "$lib/firebase";
-  import { doc, getDoc, writeBatch} from "firebase/firestore";
+<script lang='ts'>
+  import AuthCheck from '$lib/components/AuthCheck.svelte';
+  import { db, user } from '$lib/firebase';
+  import { doc, getDoc, writeBatch } from 'firebase/firestore';
 
-  let username = "";
+  let username = '';
   let loading = false;
   let isAvailable = false;
 
@@ -17,9 +17,9 @@
     loading = true;
 
     debounceTimer = setTimeout(async () => {
-      console.log("checking availability of", username);
+      console.log('checking availability of', username);
 
-      const ref = doc(db, "usernames", username);
+      const ref = doc(db, 'usernames', username);
       const exists = await getDoc(ref).then((doc) => doc.exists());
 
       isAvailable = !exists;
@@ -30,27 +30,47 @@
   }
 
   async function confirmUsername() {
-    // TODO
+    console.log('confirming username', username);
+    const batch = writeBatch(db);
+    batch.set(doc(db, 'usernames', username), { uid: $user?.uid });
+    batch.set(doc(db, 'users', $user!.uid), {
+      username,
+      photoURL: $user?.photoURL ?? null,
+      published: true,
+      bio: 'I am the Walrus',
+      links: [
+        {
+          title: 'Test Link',
+          url: 'http://localhost:5173',
+          icon: 'custom'
+        }
+      ]
+    });
+
+    await batch.commit();
+
+    username = '';
+    isAvailable = false;
+
   }
 
 </script>
 
 
-
 <AuthCheck>
   <h2>Username</h2>
-  <form class="w-2/5" on:submit|preventDefault={confirmUsername}>
+  <form class='w-2/5' on:submit|preventDefault={confirmUsername}>
     <input
-      type="text"
-      placeholder="Username"
-      class="input w-full"
+      type='text'
+      placeholder='Username'
+      class='input w-full'
       bind:value={username}
       on:input={checkAvailability}
     />
 
     <p>Is available? {isAvailable}</p>
 
-    <button class="btn btn-success">Confirm username @{username} </button>
+    <button class='btn btn-success'>Confirm username @{username} </button>
 
   </form>
 
